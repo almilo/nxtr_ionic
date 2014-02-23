99fe829fcf89c790ddeee90ea7a6e765593d776f
Alberto Mijares <almilo@almilo.es>
2014-02-23 14:20:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+NXTR - Next Train implemented with ionic and Phonegap
+=====================================================
+
+This application is based on the opendata.ch Transport API (http://http://transport.opendata.ch) and allows the user to
+visualize the schedule of the upcoming trains, tramways and buses for a specific stop.
---
+++ www/js/app.js
@@ -0,0 +1,26 @@
+angular.module('nxtr', ['ionic'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+
+    .state('tab.about', {
+      url: '/about',
+      views: {
+        'about-tab': {
+          templateUrl: 'templates/about.html'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('/tab/about');
+
+});
+
