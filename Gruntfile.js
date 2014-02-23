0f0d72d0d0da7e5c1ddc4a984e5563979038c898
Alberto Mijares <almilo@almilo.es>
2014-02-23 15:41:18 +0100

    changed structure and added grunt build mechanism

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,47 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: {
+            build: 'build'
+        },
+
+        copy: {
+            cordova_hidden: {
+                expand: true,
+                cwd: 'cordova/.cordova/',
+                src: '**',
+                dest: 'build/.cordova/'
+            },
+            cordova: {
+                expand: true,
+                cwd: 'cordova/',
+                src: '**',
+                dest: 'build/',
+                options: {
+                    mode: true
+                }
+            },
+            lib: {
+                expand: true,
+                cwd: 'app/lib/',
+                src: '**',
+                dest: 'build/www/lib/'
+            },
+            src: {
+                expand: true,
+                cwd: 'app/src/',
+                src: '**',
+                dest: 'build/www/'
+            }
+        }
     });
 
-    grunt.registerTask('default', function() {
-        var message = grunt.template.process('Application: "<%= pkg.name%>", version: <%= pkg.version%>.');
-        grunt.log.write(message);
-    });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('build', ['copy']);
+    grunt.registerTask('default', ['clean', 'build']);
 
 };
